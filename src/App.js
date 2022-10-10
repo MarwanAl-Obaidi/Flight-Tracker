b3c30e8e37797ffd43beda2567111cd5a9f99b7d
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2022-10-10 16:36:18 +0300

    API fetch added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [isReady, setReady] = useState(false);
+  const [flight_number, set_flight_number] = useState("");
+
+  useEffect(() => {
+    fetch("https://flight-tracker-api.onrender.com/api/helsinki/arrivals")
+    .then(response => response.json())
+    .then(data => {
+      setReady(true);
+      //API Fetch Test
+      set_flight_number(data.arrivals[0].flight_number);
+    })
+    .catch(err => console.log(err))
+  }, [])
+
+  if (!isReady)
+    return <div className="loading">Loading...</div>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Helsinki Airport</h1>
+      {flight_number}
     </div>
   );
 }
