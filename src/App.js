3d1acb7951b905488db0f40a73fe6f077cbbad77
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2022-10-11 16:31:35 +0300

    Basic functionality added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import './App.css';
 function App() {
   const [isReady, setReady] = useState(false);
   const [flight_number, set_flight_number] = useState("");
+  const [flight_from, set_flight_from] = useState("");
+  const [flight_to, set_flight_to] = useState("");
+  const [flight_arrival, set_flight_arrival] = useState("");
 
   useEffect(() => {
     fetch("https://flight-tracker-api.onrender.com/api/helsinki/arrivals")
     .then(response => response.json())
     .then(data => {
       setReady(true);
-      //API Fetch Test
       set_flight_number(data.arrivals[0].flight_number);
+      set_flight_from(data.arrivals[0].from);
+      set_flight_to(data.arrivals[0].to);
+      set_flight_arrival(data.arrivals[0].arrival);
     })
     .catch(err => console.log(err))
   }, [])
@@ -21,8 +26,26 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Helsinki Airport</h1>
-      {flight_number}
+      <h1>Helsinki / Arrivals</h1>
+      <p>Powered by: <a href="https://flight-tracker-api.onrender.com/">flight-tracker-api</a></p>
+      <div>
+      <table className="Table">
+        <tbody>
+          <tr>
+            <th>Flight Number: {flight_number}</th>
+          </tr>
+          <tr>
+            <td>From: {flight_from}</td>
+          </tr>
+          <tr>
+            <td>To: {flight_to}</td>
+          </tr>
+          <tr>
+            <td>Arrival: {flight_arrival}</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
     </div>
   );
 }
