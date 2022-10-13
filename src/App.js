b2839963806db63b50ac6f437cb7cd191bd183c8
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2022-10-13 04:04:02 +0300

    Updated main page to list all arrivals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [arrivals, setArrivals] = useState([]);
   const [isReady, setReady] = useState(false);
-  const [flight_number, set_flight_number] = useState("");
-  const [flight_from, set_flight_from] = useState("");
-  const [flight_to, set_flight_to] = useState("");
-  const [flight_arrival, set_flight_arrival] = useState("");
 
   useEffect(() => {
-    fetch("https://flight-tracker-api.onrender.com/api/helsinki/arrivals")
+    fetch('https://flight-tracker-api.onrender.com/api/helsinki/arrivals')
     .then(response => response.json())
-    .then(data => {
+    .then(resData => {
       setReady(true);
-      set_flight_number(data.arrivals[0].flight_number);
-      set_flight_from(data.arrivals[0].from);
-      set_flight_to(data.arrivals[0].to);
-      set_flight_arrival(data.arrivals[0].arrival);
+      setArrivals(resData.arrivals);
     })
-    .catch(err => console.log(err))
-  }, [])
+  }, []);
 
   if (!isReady)
     return <div className="loading">Loading...</div>
@@ -29,22 +22,24 @@ function App() {
       <h1>Helsinki / Arrivals</h1>
       <p>Powered by: <a href="https://flight-tracker-api.onrender.com/">flight-tracker-api</a></p>
       <div>
-      <table className="Table">
-        <tbody>
-          <tr>
-            <th>Flight Number: {flight_number}</th>
-          </tr>
-          <tr>
-            <td>From: {flight_from}</td>
-          </tr>
-          <tr>
-            <td>To: {flight_to}</td>
-          </tr>
-          <tr>
-            <td>Arrival: {flight_arrival}</td>
-          </tr>
-        </tbody>
-      </table>
+          {
+          arrivals.map((plane, index) => 
+          <table className="Table" key={index}>
+            <tr>
+              <th>Flight Number: {plane.flight_number}</th>
+            </tr>
+            <tr>
+              <td>From: {plane.from}</td>
+            </tr>
+            <tr>
+              <td>To: {plane.to}</td>
+            </tr>
+            <tr>
+              <td>Arrival: {plane.arrival}</td>
+            </tr>
+          </table>
+          )
+          }
       </div>
     </div>
   );
