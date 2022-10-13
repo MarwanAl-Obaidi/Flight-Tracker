6e214759e44cc7077a78659852ce73709c7532e5
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2022-10-13 05:53:20 +0300

    Pages and routing added

---
+++ src/pages/NoPage.jsx
@@ -0,0 +1,10 @@
+function NoPage() {
+    return (
+        <div>
+            <h1>There is nothing here buddy</h1>
+            <a href="/">Return Home</a>
+        </div>
+    );
+}
+
+export default NoPage;
