f1fa54fd299735d9dee24a191c17d44a4408feaf
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2022-10-15 23:00:35 +0300

    Bug fix attempt two

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const Home = () => (
   <div className='App'>
     <h1>Flight Tracker</h1>
     <p>Main Page</p>
+    <Link to="/hel">Helsinki</Link>
   </div>
 )
 
@@ -37,6 +38,7 @@ function Helsinki() {
     <div className="App">
       <h1>Helsinki / Arrivals</h1>
       <p>Powered by: <a href="https://flight-tracker-api.onrender.com/">flight-tracker-api</a></p>
+      <Link to="/">Back</Link>
       <div>
           {
           arrivals.map((plane, index) => 
